6fbecd0aa597a046842f4f7a53b43bad0d0f68d8
Daffa Shidiq <daffashi00@gmail.com>
2023-06-05 16:54:06 +0700

    add dynamic header

--- src/components/TableTemp.tsx
+++ src/components/TableTemp.tsx
@@ -1,25 +1,21 @@
-const TableTemp = () => {
+interface TableTempProps {
+  headers: string[]
+}
+
+const TableTemp = ({ headers } : TableTempProps) => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="max-w-full overflow-x-auto">
         <table className="w-full table-auto">
           <thead>
             <tr className="bg-gray-2 text-left dark:bg-meta-4">
-              <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
-                Nama Buku
-              </th>
-              <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
-                Penerbit
-              </th>
-              <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
-                Tahun Terbit
-              </th>
-              <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
-                Stok
-              </th>
-              <th className="py-4 px-4 font-medium text-black dark:text-white">
-                Actions
-              </th>
+              {headers?.map((v) => {
+                return (
+                  <th className="py-4 px-4 font-medium text-black dark:text-white">
+                    {v}
+                  </th>
+                )
+              })}
             </tr>
           </thead>
           <tbody>
@@ -28,7 +24,6 @@ const TableTemp = () => {
                 <h5 className="font-medium text-black dark:text-white">
                   Free Package
                 </h5>
-                <p className="text-sm">$0.00</p>
               </td>
               <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                 <p className="text-black dark:text-white">Jan 13,2023</p>
