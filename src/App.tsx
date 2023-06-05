4bb6ebf0e5b493832329277132d94ae92c5c7277
Daffa Shidiq <daffashi00@gmail.com>
2023-06-05 10:32:56 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import FormElements from './pages/Form/FormElements';
+import FormLayout from './pages/Form/FormLayout';
+import Tables from './pages/Tables';
+import Alerts from './pages/UiElements/Alerts';
+import Buttons from './pages/UiElements/Buttons';
+
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const preloader = document.getElementById('preloader');
+
+  if (preloader) {
+    setTimeout(() => {
+      preloader.style.display = 'none';
+      setLoading(false);
+    }, 2000);
+  }
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <p className=" text-center text-danger">Failed to lead app</p>
+  ) : (
+    <>
+      <Routes>
+        <Route path="/forms/form-elements" element={<FormElements />} />
+        <Route path="/forms/form-layout" element={<FormLayout />} />
+        <Route path="/tables" element={<Tables />} />
+        <Route path="/ui/alerts" element={<Alerts />} />
+        <Route path="/ui/buttons" element={<Buttons />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
